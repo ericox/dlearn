d9d8225bf52eadd00ad76f5be56397d19d9277b8
Eric Cox <emcox84@gmail.com>
2016-05-27 00:02:55 -0400

    adding medium sized runs

--- grpc_benchmarks/run_send_buffer.sh
+++ grpc_benchmarks/run_send_buffer.sh
@@ -8,8 +8,14 @@ do
       $BENCHPATH/bufservice_client $ws 1000 >> result
 done
 
+# medium runs
+for ws in 16384 32768 65536 262144
+do
+      $BENCHPATH/bufservice_client $ws 100 >> result
+done
+
 # large runs
 for ws in 2097152 8388608 33554432 134217728 268435456
 do
-    $BENCHPATH/bufservice_client $ws 2 >> result
+    $BENCHPATH/bufservice_client $ws 3 >> result
 done
