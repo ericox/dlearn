c5893d6acf8d604dbe77b3d344803da3a5e942ae
Eric Cox <emcox84@gmail.com>
2016-06-02 15:36:48 -0400

    updating run script

--- grpc_benchmarks/run_send_buffer.sh
+++ grpc_benchmarks/run_send_buffer.sh
@@ -3,19 +3,8 @@
 BENCHPATH=./buffer
 
 # small runs
-for ws in 256 512 1024 2048 4096 8192 
+for ws in 256 512 1024 2048 4096 8192 16384 32768 65536 262144 2097152 8388608 33554432 134217728 268435456
 do
       $BENCHPATH/bufservice_client $ws 1000 >> result
 done
 
-# medium runs
-for ws in 16384 32768 65536 262144
-do
-      $BENCHPATH/bufservice_client $ws 100 >> result
-done
-
-# large runs
-for ws in 2097152 8388608 33554432 134217728 268435456
-do
-    $BENCHPATH/bufservice_client $ws 3 >> result
-done
